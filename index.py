a4d9165fe9307feeb519be08a632fe11562e515c
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-07-19 22:18:11 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
 num = int(input('Dígite um número para análise de ímpar/par: '))
+def analise():
+    if num % 2 == 0:
+        print('Seu Número é par!')
+    else:
+        print('Seu Número é impar!')
+
+analise()
+
+
